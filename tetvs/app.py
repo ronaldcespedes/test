a5fb7d3479756c40897136b7ceef8907d5e3a37e
Ronald Cespedes <rcespedes@gammacargo.com>
2024-06-20 10:20:56 -0500

    versiontest

---
+++ tetvs/app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+
+    return "<p>Hello, World!</p>"
+
+
+@app.route("/datos_agentes")
+def datos_agentes():
+    
+    
+    return "<p>datos agentes!</p>"
